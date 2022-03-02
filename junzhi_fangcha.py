1a83f2a225b797e96e90b32478bb0e11185384ec
aimen123 <1275289403@qq.com>
2022-03-02 11:13:21 +0800

    mean-std-4

---
+++ junzhi_fangcha.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2
+import os
+from glob import glob
+#from IPython import embed
+from tqdm import tqdm
+import PIL
+from PIL import Image
+
+# img_h, img_w = 32, 32
+img_h, img_w = 416, 416  # 根据自己数据集适当调整，影响不大
+means, stdevs = [], []
+img_list = []
+
+TRAIN_DATASET_PATH = 'val'
+
+
+image_fns = glob(os.path.join(TRAIN_DATASET_PATH, '*', '*.*'))
+
+for single_img_path in tqdm(image_fns):
+    #img = cv2.imread(single_img_path)
+    #img = cv2.resize(img, (img_w, img_h))
+    img = Image.open(single_img_path)
+    img = img.resize((img_h,img_w))
+    img = np.asarray(img)
+    img = img[:, :, :, np.newaxis]
+    img_list.append(img)
+
+
+imgs = np.concatenate(img_list, axis=3)
+imgs = imgs.astype(np.float32) / 255.
+
+for i in range(4):
+    pixels = imgs[:, :, i, :].ravel()  # 拉成一行
+    means.append(np.mean(pixels))
+    stdevs.append(np.std(pixels))
+
+# BGR --> RGB ， CV读取的需要转换，PIL读取的不用转换
+#means.reverse()
+#stdevs.reverse()
+
+print("normMean = {}".format(means))
+print("normStd = {}".format(stdevs))
+
